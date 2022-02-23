1dab6b569995a0ae0c798a192f23ebf607ac889b
Bel <cristinna.cristinna@gmail.com>
2022-02-23 15:22:56 -0300

    first commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -4,7 +4,44 @@ import br.com.dio.model.Gato;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-        Gato gato = new Gato();
-        System.out.println(gato);
+        //Gato gato = new Gato("teste");
+       // System.out.println(gato);
+
+        Livro livro = new Livro("Tstesss", 388);
+        System.out.println(livro);
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
